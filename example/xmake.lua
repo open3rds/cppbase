330c7002760a6c7b50e3c47dfe910081747712e1
Pan <18226542+rzbdz@users.noreply.github.com>
2022-11-27 11:41:22 +0000

    fix git ssh

--- example/xmake.lua
+++ example/xmake.lua
@@ -1,4 +1,4 @@
-add_repositories("open3rds git@github.com:open3rds/xmake-repo.git main")
+add_repositories("open3rds https://github.com/open3rds/xmake-repo main")
 -- add_repositories("open3rds git@github.com:open3rds/xmake-repo.git experiment")
 add_requires("open3rds::cppbase  1.0.x", {alias = "cppbase"})
 
